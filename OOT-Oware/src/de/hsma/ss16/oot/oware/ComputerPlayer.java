2815470323246ab29a8c2585bb88b1d9ec2cef3b
Tobias <Tobias@Tobias-PC.Speedport_W_724V_Typ_A_05011603_00_009>
2016-06-09 14:01:11 +0200

    Added attribute difficulty to ComputerPlayer

--- OOT-Oware/src/de/hsma/ss16/oot/oware/ComputerPlayer.java
+++ OOT-Oware/src/de/hsma/ss16/oot/oware/ComputerPlayer.java
@@ -2,14 +2,20 @@ package de.hsma.ss16.oot.oware;
 
 class ComputerPlayer extends Player {
 	private static String NAME = "Computer";
+	private int difficulty;
 
-	public ComputerPlayer() {
+	public ComputerPlayer(int difficulty) {
 		super(NAME);
+		this.difficulty = difficulty;
 	}
 
 	@Override
 	Draw doDraw(Pitch pitch) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	int getDifficulty() {
+		return this.difficulty;
 	}
 }

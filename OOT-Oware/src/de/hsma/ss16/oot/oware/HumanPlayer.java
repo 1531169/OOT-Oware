a4284bcf5c609dd0c484ba5f263c0cd699e3714f
Tobias <Tobias@Tobias-PC.Speedport_W_724V_Typ_A_05011603_00_009>
2016-06-09 10:10:56 +0200

    Added attribute InputMap to HumanPlayer

--- OOT-Oware/src/de/hsma/ss16/oot/oware/HumanPlayer.java
+++ OOT-Oware/src/de/hsma/ss16/oot/oware/HumanPlayer.java
@@ -1,14 +1,23 @@
 package de.hsma.ss16.oot.oware;
 
+import java.util.HashMap;
+
 class HumanPlayer extends Player {
 
-	public HumanPlayer(String name) {
+	private HashMap<Character, Integer> inputMap;
+	
+	public HumanPlayer(String name, HashMap<Character, Integer> inputMap) {
 		super(name);
+		this.inputMap = inputMap;
 	}
 
 	@Override
 	Draw doDraw(Pitch pitch) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	HashMap<Character, Integer> getInputMap() {
+		return this.inputMap;
 	}
 }

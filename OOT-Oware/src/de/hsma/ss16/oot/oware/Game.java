0ec36ec9b49bbe4c2a46766ddc4a7dc715cce632
Tobias <Tobias@Tobias-PC.Speedport_W_724V_Typ_A_05011603_00_009>
2016-06-09 15:07:42 +0200

    Implemented and updated class Game

--- OOT-Oware/src/de/hsma/ss16/oot/oware/Game.java
+++ OOT-Oware/src/de/hsma/ss16/oot/oware/Game.java
@@ -1,5 +1,8 @@
 package de.hsma.ss16.oot.oware;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class Game {
 	private static final Pitch pitch = new Pitch();
 	private static Player player1;
@@ -7,33 +10,38 @@ class Game {
 	private static Player onWait;
 	private static Player onDraw;
 	private static Player winner;
-	private static boolean isRunning;
+	private static boolean isFinished;
+	private static HashMap<Character, Integer> inputMap1;
+	private static HashMap<Character, Integer> inputMap2;
+	private static ArrayList<Draw> drawlist;
 
 	public Game() {
 		init();
+		play();
 	}
 
-	void init() {
-		// TODO: Abfrage in Konsole
-		Mode inGameMode = Mode.PVC;
-		String inPlayer1 = "Cedric";
-		String inPlayer2 = "Tobias";
-		isRunning = true;
+	private void init() {
+		initMaps();
+		drawlist = new ArrayList<>();
+		isFinished = false;
+		IOController.printWelcome();
+		Mode inGameMode = IOController.getMode();
 
 		switch (inGameMode) {
 			case PVC:
-				player1 = new ComputerPlayer();
-				player2 = new HumanPlayer(inPlayer2);
-				player2.setRange(6, 12);
-				break;
-			case PVP:
-				player1 = new HumanPlayer(inPlayer1);
-				player2 = new HumanPlayer(inPlayer2);
+				IOController.printPlayer1();
+				player1 = new HumanPlayer(IOController.getName(), inputMap1);
+				player2 = new ComputerPlayer(IOController.getDifficulty());
 				player1.setRange(0, 6);
 				player2.setRange(6, 12);
 				break;
-			default:
-				// TODO: throw exception
+			case PVP:
+				IOController.printPlayer1();
+				player1 = new HumanPlayer(IOController.getName(), inputMap1);
+				IOController.printPlayer2();
+				player2 = new HumanPlayer(IOController.getName(), inputMap2);
+				player1.setRange(0, 6);
+				player2.setRange(6, 12);
 				break;
 		}
 
@@ -47,29 +55,81 @@ class Game {
 			setOnWait(player1);
 			setOnDraw(player2);
 		}
-
-		while (isRunning) {
-			//onDraw.doDraw(getPitch());
-			System.out.println(onDraw.getName());
+	}
+	
+	private void play() {
+		while(!isFinished) {
+			IOController.printPitch();
+			playTurn(onDraw);
 			nextPlayer();
 		}
+		showResult();
+		anotherRound();
+	}
+	
+	private void playTurn(Player player) {
+		IOController.printOnDraw(player);
+		drawlist.add(player.doDraw());
+		checkFinished(player);
 	}
 	
 	public static void main(String[] args) {
 		new Game();
 	}
-
+	
 	/**
 	 * wechselt Spieler
 	 */
 	void nextPlayer() {
-		Player tmpPlayer = onDraw;
+		Player tmpPlayer = getOnDraw();
 		setOnDraw(getOnWait());
 		setOnWait(tmpPlayer);
 	}
 	
+	private static void checkFinished(Player player) {
+		if(player.getPoints() >= 25) {
+			setWinner(player);
+		}
+	}
+	
+	private static void showResult() {
+		if(getWinner() == null) {
+			IOController.printTie();
+		}
+		else{
+			IOController.printWinner(getWinner());
+		}
+	}
+	
+	private void anotherRound() {
+		if(IOController.getAnotherRound()) {
+			reset();
+		}
+	}
+	
 	void reset() {
 		init();
+		play();
+	}
+		
+	private void initMaps() {
+		inputMap1 = new HashMap<>();
+		inputMap2 = new HashMap<>();
+		
+		inputMap1.put('A', 0);
+		inputMap1.put('B', 1);
+		inputMap1.put('C', 2);
+		inputMap1.put('D', 3);
+		inputMap1.put('E', 4);
+		inputMap1.put('F', 5);
+		
+		inputMap2.put('a', 6);
+		inputMap2.put('b', 7);
+		inputMap2.put('c', 8);
+		inputMap2.put('d', 9);
+		inputMap2.put('e', 10);
+		inputMap2.put('f', 11);
+		
 	}
 
 	public static Player getOnDraw() {
@@ -90,5 +150,14 @@ class Game {
 
 	public static void setOnWait(Player onWait) {
 		Game.onWait = onWait;
+	}
+	
+	public static Player getWinner() {
+		return winner;
+	}
+	
+	private static void setWinner(Player player) {
+		winner = player;
+		isFinished = true;
 	}
 }

cbc785d012a8106f8c8a15a605b64bea6c74ac13
Igor <Igor@IGOR>
2016-06-16 19:11:49 +0200

    g

---
+++ OOT-Oware/src/de/hsma/ss16/oot/oware/DrawTest.java
@@ -0,0 +1,20 @@
+package de.hsma.ss16.oot.oware;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DrawTest {
+
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsDrawValid() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ OOT-Oware/src/de/hsma/ss16/oot/oware/GameTest.java
@@ -0,0 +1,19 @@
+package de.hsma.ss16.oot.oware;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void testGame(){
+	
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}

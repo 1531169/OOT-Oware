23f6dfdfeea03bc39024cc86f5dee00e25c26923
MayVanEs-3 <maydah.ahmed@t-online.de>
2016-06-17 13:17:53 +0200

    ComputerPlayerTest*n

---
+++ OOT-Oware/src/de/hsma/ss16/oot/oware/ComputerPlayerTest.java
@@ -0,0 +1,14 @@
+package de.hsma.ss16.oot.oware;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputerPlayerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
